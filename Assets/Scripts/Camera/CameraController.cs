8367f3ce995411b6594ec4a6b77d57471b7ae2ec
EmilyAIE <s213853@students.aie.edu.au>
2025-01-25 20:48:32 +1030

    Added player controller and TMP

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraController : MonoBehaviour
+{
+
+    public ArenaGameInput cameraControls;
+    private InputAction m_look;
+
+    [Header("Camera Details")]
+    [SerializeField] private float m_cameraSpeed;
+    private Vector2 m_cameraDirection = Vector2.zero;
+    [SerializeField] private float m_verticalClamp = 80f;
+    private float m_cameraVerticalRotation;
+    private float m_cameraHorizontalRotation;
+
+    [Space]
+    [SerializeField] private Transform m_player;
+
+    private PlayerStatus m_playerStatus;
+
+    private void Awake()
+    {
+        //Initialise the input actions
+        cameraControls = new ArenaGameInput();
+    }
+
+    private void OnEnable()
+    {
+        m_look = cameraControls.Player.Look;
+        m_look.Enable();
+
+        m_playerStatus = GetComponentInParent<PlayerStatus>();
+    }
+
+    private void OnDisable()
+    {
+        m_look.Disable();
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+        if (Time.timeScale == 1 && m_playerStatus.isAlive)
+        {
+            //Update camera direction vector
+            m_cameraDirection = m_look.ReadValue<Vector2>() * m_cameraSpeed;
+
+            //Adjust the value of the camera vertical rotation
+            m_cameraVerticalRotation -= m_cameraDirection.y;
+
+            //Clamp vertical movement so to avoid gimblelock issues
+            m_cameraVerticalRotation = Mathf.Clamp(m_cameraVerticalRotation, -m_verticalClamp, m_verticalClamp);
+
+            //Set the value of the vertical roation
+            transform.localEulerAngles = Vector3.right * m_cameraVerticalRotation;
+
+            //Set the value of the horizontal rotation
+            m_player.Rotate(Vector3.up * m_cameraDirection.x);
+        }
+        else
+            return;
+    }
+}
